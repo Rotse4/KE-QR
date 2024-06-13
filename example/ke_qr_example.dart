6ad1d7949c0451c18e20186aa2fe87adfc0ef5d7
Japheth <muusyajapheth12@gmail.com>
2024-06-13 13:00:13 +0300

    specified data requirements eg M,O,C

--- example/ke_qr_example.dart
+++ example/ke_qr_example.dart
@@ -12,28 +12,33 @@ void main() {
   Process process = Process();
   var data=process.processQrCode("00020101021128184614460120002574375303404540597.005802KE5914KONNECT WALLET6007NAIROBI61020082262024-06-12 17:28:40.7381586304ED73");
   print(data.transactionAmount?.value);
+
+  //for ids with more than one value eg paymentadress you will access the list like shown below
+  print(data.paymentAddress?.value?[0].value);
+
+  //incase the qr code has been altered it will return null
   print(data.crc?.value);
 
 
   /* BUILDING QR CODE */
 
   QrBuild qrBuild = QrBuild();
-
+    //The uncommented data is Mandatory
     Map<String, String> buildingData = {
     "00": "01", // Don't change this
     "01": "11", // switch beween static and dynamic 11/12
     // "02": "12345678", // global PSPs visa mastercard etc 
-    "28": "46012000257437",//paymentAddress
+    "28": "******",//paymentAddress
     // "52": "4900",
     "53": "404", 
-    "54": "297.00",
+    // "54": "297.00",
     // "55": "",
     // "56": "",
     // "57": "",
     "58": "KE",
-    "59": "KONNECT WALLET",
-    "60": "NAIROBI",
-    "61": "00",
+    "59": "******",
+    // "60": "NAIROBI",
+    // "61": "00",
     // "62": "030800112349070511002",
     // "64": "",
     // "65": "",
